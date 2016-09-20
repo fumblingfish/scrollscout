b992a6b097adb4709d2aa1ec76c9e9ce73e1b0a3
Rasmus Andersson <holstandersson@gmail.com>
2016-09-20 21:16:29 +0200

    refactor pinbase

--- src/pinbase.js
+++ src/pinbase.js
@@ -1,7 +1,8 @@
 import _ from 'lodash'
 
+const exists = _.negate(_.isNil)
+
 var parseOffset = function (offset) {
-   if (offset == null) return 0
    return _.isString(offset) ? parseFloat(offset.match(/[-+0-9]+/g)) : parseFloat(offset)
 }
 
@@ -18,16 +19,17 @@ class PinBase {
    }
 
    view(position, offset) {
-      this._view.position = _.isNumber(position) ? position : this._view.position
-      this._view.offset = parseOffset(offset)
+      this._view.position = (exists(position) && _.isNumber(position)) ? position : this._view.position
+      this._view.offset = exists(offset) ? parseOffset(offset) : this._view.offset
       return this
    }
 
    scene(position, offset) {
-      this._scene.position = _.isNumber(position) ? position : this._scene.position
-      this._scene.offset = parseOffset(offset)
+      this._scene.position =  (exists(position) && _.isNumber(position)) ? position : this._scene.position
+      this._scene.offset = exists(offset) ? parseOffset(offset) : this._scene.offset
       return this
    }
 }
+
 
 export default PinBase
